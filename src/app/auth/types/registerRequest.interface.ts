1b0db560d0ca2a5e37813a10efdf2c1eccfc43a2
Denis Lopatin <stalk.sdv@gmail.com>
2021-10-20 14:34:02 +0200

    added types for auth module

---
+++ src/app/auth/types/registerRequest.interface.ts
@@ -0,0 +1,5 @@
+export interface RegisterRequestInterface {
+  user: string
+  email: string
+  password: string
+}
---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,10 @@
+export interface CurrentUserInterface {
+  id: number
+  email: string
+  username: string
+  bio: string | null
+  createdAt: string
+  updatedAt: string
+  image: string | null
+  token: string
+}
