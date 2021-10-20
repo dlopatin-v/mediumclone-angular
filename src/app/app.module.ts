0017422803e4241d238c2c48d8eca14f0d36de29
Denis Lopatin <stalk.sdv@gmail.com>
2021-10-20 13:27:51 +0200

    added ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from 'src/environments/environment'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -8,7 +11,18 @@ import { TestModule } from './test/test.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, TestModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    TestModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
